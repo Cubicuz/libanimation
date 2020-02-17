de06ad71758fa1f28a323787263e40984df0d1c0
cubicuz <no@mail.me>
2020-02-17 22:16:58 +0100

    rainbow working

--- include/animation.h
+++ include/animation.h
@@ -23,18 +23,55 @@ private:
   uint8_t state [ledCount*3];
   uint32_t speed;
   uint32_t resolution;
+
 public:
-  void animate();
+
+  void animate(){
+    rainbow();
+  }
   void setAnimation(void (*fkt));
   void setAnimation(const char* name);
   void setAnimation(int index);
 
+  char ** getAvailableAnimations();
 
   uint32_t getLedCount();
   uint32_t getSpeed();
   void setSpeed(uint32_t speed);
   uint32_t getResolution();
   void setResolution(uint32_t);
+  void setSetPixelColor(void (*setPixelColor)(uint32_t, uint8_t, uint8_t, uint8_t)){
+    this->setPixelColor = setPixelColor;
+  }
+
+private:
+  uint8_t wheel(uint16_t x, uint16_t resolution){
+    // function looks like ／￣￣＼＿＿
+    // is splittet in 4 parts (rising 0, high 1-2, falling 3, low 4-5)
+
+    if (x*6 < resolution)           // x < 1/6 * resolution
+    { // rising
+      return 255*6*x/resolution/2;   // 255 * x / (resolution / 6)
+    } else if (x*2 < 1*resolution)  // x < 3/6 * resolution
+    { // high
+      return 255;
+    } else if (x*3 < 2*resolution)  // x < 4/6 * resolution
+    { // falling
+      return (4*255 - 6*x*255/resolution)/2 ;
+    } else 
+    { // low
+      return 0;
+    }
+  }
+
+  void rainbow(){
+    for (uint32_t i = 0; i < ledCount; i++){
+      setPixelColor(i, wheel(i, ledCount), wheel((i+ledCount/3)%ledCount, ledCount), wheel((i+ledCount*2/3)%ledCount, ledCount));
+    }
+  }
+
+
+
 };
 
 // some kind of this will be needed
--- src/main.cpp
+++ src/main.cpp
@@ -4,66 +4,36 @@
 #include "animation.h"
 
 #define NUM_LEDS 30
-#define DATA_PIN 5 // this is D1 because of reasons
+#define DATA_PIN 0 
 
 Adafruit_NeoPixel strip(NUM_LEDS, DATA_PIN, NEO_GRBW + NEO_KHZ800);
 
-uint8_t wheel(uint16_t x, uint16_t resolution);
 
-containingFixedsizeArray<17> b;
+AnimatedStrip<29> animStrip;
+
+void mySetPixelColor(uint32_t i, uint8_t r, uint8_t g, uint8_t b){
+  strip.setPixelColor(i, r, g, b, 0);
+  Serial.print(r);
+  Serial.print(" ");
+  Serial.print(g);
+  Serial.print(" ");
+  Serial.println(b);
+}
 
 void setup()
 {
-  a.stuff[15] = 14;
-  b.stuff[1] = 16;
   // put your setup code here, to run once:
   Serial.begin(19200);
   Serial.println("Start");
   strip.begin();
-  strip.show();
-  strip.setPixelColor(1, 255, 0, 0, 0);
-  strip.setPixelColor(2, 0, 255, 0, 0);
-  strip.setPixelColor(3, 0, 0, 255, 0);
-  strip.setPixelColor(4, 0, 0, 0, 255);
-  strip.show();
-  for (uint16_t i=0;i<NUM_LEDS;i++){
-    uint8_t red = wheel(i, NUM_LEDS);
-    strip.setPixelColor(i, red, wheel((i+10)%NUM_LEDS, NUM_LEDS), wheel((i+20)%NUM_LEDS, NUM_LEDS), 0);
-    Serial.print(i);
-    Serial.print(" : ");
-    Serial.println(red);
-  }
+  animStrip.setSetPixelColor(&mySetPixelColor);
+  animStrip.animate();
   strip.show();
 
-  Serial.println(a.stuff[15]);
-  Serial.println(b.stuff[1]);
+
 }
 
 void loop()
 {
 }
 
-uint8_t wheel(uint16_t x, uint16_t resolution){
-  
-  // function looks like ／￣￣＼＿＿
-  // is splittet in 4 parts (rising 0, high 1-2, falling 3, low 4-5)
-
-  if (x*6 < resolution)           // x < 1/6 * resolution
-  { // rising
-    return 255*6*x/resolution/2;   // 255 * x / (resolution / 6)
-  } else if (x*2 < 1*resolution)  // x < 3/6 * resolution
-  { // high
-    return 255;
-  } else if (x*3 < 2*resolution)  // x < 4/6 * resolution
-  { // falling
-    return (4*255 - 6*x*255/resolution)/2 ;
-  } else 
-  { // low
-    return 0;
-  }
-  
-}
-
-void rainbow(){
-
-}
